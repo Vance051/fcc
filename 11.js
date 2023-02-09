c9b55671966cc88b10770e9f391f6bc3b2f43234
Vance051 <vance051@gmail.com>
2023-02-10 01:36:09 +0300

    add task Concatenating Strings with Plus Operator1

---
+++ 11.js
@@ -0,0 +1,16 @@
+// Escape Sequences in Strings
+// Code	Output
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \t	tab
+// \r	carriage return
+// \b	word boundary
+// \f	form feed
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+// // Concatenating Strings with Plus Operator
+// const myStr = "This is the start. " + "This is the end."; // Change this line
