b16fe26f9fa83b086b3ade0b3cd8990d1ba2a3b4
Vance051 <vance051@gmail.com>
2023-02-10 00:58:01 +0300

    add task  Increment\Decrement, Decimal Numbers

---
+++ 8.js
@@ -0,0 +1,18 @@
+// Increment a Number with JavaScript
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+// Decrement a Number with JavaScript
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+// Create Decimal Numbers with JavaScript
+
+// Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
