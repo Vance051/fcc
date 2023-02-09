79297c179b421e1563b8ec97f5df01655e212d15
Vance051 <vance051@gmail.com>
2023-02-10 01:23:50 +0300

    add task Quoting Strings

---
+++ 10.js
@@ -0,0 +1,8 @@
+// Escaping Literal Quotes in Strings
+// backslash
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+// Quoting Strings with Single Quotes
+
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
