6fc66fd75200d7b2e666ac4e72b78d9b6d7704f8
Vance051 <vance051@gmail.com>
2023-02-10 19:34:14 +0300

    add task function

---
+++ JS/15.js
@@ -0,0 +1,12 @@
+// function
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
+
+//   Params
+function functionWithArgs(a, b) {
+  console.log(+a + +b);
+}
+functionWithArgs(4, 5);
