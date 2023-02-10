ecf82a23d60d32a704b0acc73aa0cdff15a2de81
Vance051 <vance051@gmail.com>
2023-02-11 01:09:56 +0300

    start tasks BOOLEAN

---
+++ JS/19.js
@@ -0,0 +1,23 @@
+// Boolean
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if (wasThatTrue) {
+    return 'Yes, that was true'
+  }
+  return 'No, that was false'
+  
+    // Only change code above this line
+  
+  }
+
+//   Comparison with the Equality Operator
+  // Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
