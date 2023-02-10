0fe07c801f975a24c50e8e627476cb2e35147fa1
Vance051 <vance051@gmail.com>
2023-02-11 01:34:16 +0300

    add task Logical Order in If Else Statements

--- JS/19.js
+++ JS/19.js
@@ -65,3 +65,17 @@ function testLogicalOr(val) {
   }
   
   testLogicalOr(15);
+
+
+//   Logical Order in If Else Statements
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  orderMyLogic(7);
