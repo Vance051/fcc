cace629b30c95bc2c99e7e320b0bef55370514c3
Vance051 <vance051@gmail.com>
2023-02-11 00:51:36 +0300

    add task Understanding Undefined Value returned from a Function

--- JS/15.js
+++ JS/15.js
@@ -37,3 +37,17 @@ function fun2() {
   }
   console.log(output);
 }
+
+
+// ----- Global vs. Local Scope in Functions
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+const outerWear = 'sweater'
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
