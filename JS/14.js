d6033c976ad3e64089ece7b79cfa7418e687c303
Vance051 <vance051@gmail.com>
2023-02-10 19:24:56 +0300

    add task shopping list 14.js

---
+++ JS/14.js
@@ -0,0 +1,9 @@
+// shopping FileList
+
+
+const myList = [];
+myList.push(["Chocolate Bar", 15])
+myList.push(['Bacaley', 5])
+myList.push(['Milk', 5])
+myList.push(['Meat', 3])
+myList.push(['Cola', 6])
