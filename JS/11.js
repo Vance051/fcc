2699fb03b40b09bfbabd1468f3a1ac218451f9a5
Vance051 <vance051@gmail.com>
2023-02-10 15:02:05 +0300

    add task string length in FCC

--- JS/11.js
+++ JS/11.js
@@ -9,8 +9,16 @@
 // \b	word boundary
 // \f	form feed
 
-const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
 
 // // Concatenating Strings with Plus Operator
 // const myStr = "This is the start. " + "This is the end."; // Change this line
+
+// let myStr = 'This is the first sentence. ';
+// myStr += 'This is the second sentence.';
+
+// Only change code below this line
+const myName = "Ivan";
+const myStr = "My name is " + myName + ' and I am well!!';
+
---
+++ JS/12.js
@@ -0,0 +1,7 @@
+// String length
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
