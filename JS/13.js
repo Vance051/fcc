66c14671affd4136d425c5692c0bf59a988fd822
Vance051 <vance051@gmail.com>
2023-02-10 15:30:14 +0300

    add some tasks of array in FCC. 13.js

--- JS/13.js
+++ JS/13.js
@@ -1,1 +1,18 @@
 // Arrays
+// Only change code below this line
+const myArray = ["ok", 4, 10, "cool"];
+
+// Only change code below this line
+const myArra = [[1,2], 3];
+
+// Access Array Data with Indexes
+const myAray = [50, 60, 70];
+
+const myData = myAray[0]
+
+// Modify Array Data With Indexes
+// // Setup
+// const myArray = [18, 64, 99];
+
+// // Only change code below this line
+// myArray[0] = 45
