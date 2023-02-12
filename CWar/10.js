902a6ea066ff776a23e4b5e464957acb959b38b8
Vance051 <vance051@gmail.com>
2023-02-13 00:54:11 +0300

    add task Vowel Count at CodeWars

---
+++ CWar/10.js
@@ -0,0 +1,17 @@
+// Vowel Count
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+ 
+    let count = 0;
+    for (let val of str) {
+      if (val === 'a' || val === 'e' || val === 'i' || val === 'o' || val === 'u' ) {
+        count +=1;
+      }
+    }
+    return count;
+  }
