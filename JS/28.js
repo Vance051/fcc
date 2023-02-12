8c76ca0ae7bd3b3d99c9ce2885f3f6747b852802
Vance051 <vance051@gmail.com>
2023-02-12 22:43:44 +0300

    add task Manipulating Complex Objects

---
+++ JS/28.js
@@ -0,0 +1,27 @@
+// Manipulating Complex Objects
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+  myMusic.push({
+    "artist": "Neuro Monk",
+      "title": "Nebo",
+      "release_year": 2021,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": false
+  })
