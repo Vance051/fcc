06f3ba33b22295aef5371bd8505349bd8254caf6
Vance051 <vance051@gmail.com>
2023-02-14 00:38:27 +0300

    add task Generate Random Whole Numbers within a Range at FCC

--- JS/33.js
+++ JS/33.js
@@ -18,4 +18,15 @@ function randomFraction() {
   
     return Math.floor(Math.random() * 10);
   ;
-  }
+    }
+
+    // ===========================Generate Random Whole Numbers within a Range
+
+
+
+    function randomRange(myMin, myMax) {
+      // Only change code below this line
+      let random = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
+      return random;
+      // Only change code above this line
+    }
