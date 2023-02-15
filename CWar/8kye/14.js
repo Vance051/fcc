affa2af1b1d500c4f1e4fc07616942f0b82be0ea
Vance051 <vance051@gmail.com>
2023-02-16 01:04:30 +0300

    add task Training JS #6: Basic data types--Boolean and conditional statements if..else

---
+++ CWar/8kye/14.js
@@ -0,0 +1,5 @@
+// Training JS #6: Basic data types--Boolean and conditional statements if..else --8
+function trueOrFalse(val){
+    if (val)    return 'true' ;             
+    else     return 'false';
+  }
