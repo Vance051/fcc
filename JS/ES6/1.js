7ee12d07dcbec44bae70bd107028641cbe212513
Vance051 <vance051@gmail.com>
2023-02-16 00:09:33 +0300

    add task Mutate an Array Declared with const

---
+++ JS/ES6/1.js
@@ -0,0 +1,20 @@
+// =========================================Compare Scopes of the var and let Keywords============================================
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+    let  i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+//   ================================Mutate an Array Declared with const=========================================================
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+s[0] =2;
+s[1] =5;
+s[2] =7;
+}
+editInPlace();
