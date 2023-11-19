cc3b83d8ea233665d1bb09a704f5d1b3e4a0d159
IvanVance <vance051@gmail.com>
2023-11-20 01:10:05 +0300

    add task 'Calculate average' to CodeWars

--- CWar/8kye/36.js
+++ CWar/8kye/36.js
@@ -5,5 +5,7 @@ Write a function which calculates the average of the numbers in a given list.
     Note: Empty arrays should return 0.*/
 
 function findAverage(array) {
-    return (array.reduce((sum, acc)=> sum+acc, 0)/array.length);
+    if (array.length) {
+        return (array.reduce((sum, acc)=> sum+acc, 0)/array.length)}
+    return 0
 }
