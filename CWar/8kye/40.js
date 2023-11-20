8ae1effc7dcc5fbdd578f54d1797943b87bd82a2
IvanVance <vance051@gmail.com>
2023-11-21 01:24:28 +0300

    add task 'Are You Playing Banjo?' to CodeWars

---
+++ CWar/8kye/40.js
@@ -0,0 +1,16 @@
+/*
+Are You Playing Banjo?
+
+    Create a function which answers the question "Are you playing banjo?".
+    If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+    The function takes a name as its only argument, and returns one of the following strings:
+*/
+
+
+
+
+    function areYouPlayingBanjo(name) {
+    if (name.toUpperCase()[0]=== 'R'){
+        return `${name} plays banjo`} return `${name} does not play banjo` ;
+}
